4404e0ff832a0787a11bd3bd8a0e64927fc05dbc
Brian McEntee <brian.m.mcentee@gmail.com>
2014-06-26 14:18:06 -0400

    changed ggpf method in revised version

--- Problem_3_rev.java
+++ Problem_3_rev.java
@@ -2,16 +2,9 @@ class Problem_3_rev{
 	public final static long MAX_NUMBER =  600851475143L;
 	public static void main(String [] args){
 
-		long largestPF = MAX_NUMBER;
+		System.out.println(greatest_prime_factor(MAX_NUMBER));
+        
 
-		for(long testNum = MAX_NUMBER; testNum > 1; testNum -=2){
-			if(MAX_NUMBER % testNum == 0 & is_prime(testNum)){
-				largestPF = testNum;
-				break;
-			}
-		}
-
-		System.out.println(largestPF);
 
 	}
 	public static boolean is_prime(long testNum){
@@ -33,9 +26,26 @@ class Problem_3_rev{
         }
     }
 
-    public static boolean is_divisible(long testNum){
+    public static boolean is_divisible(long divisor, long dividend){
         boolean status = false;
-        if(MAX_NUMBER % testNum == 0) status = true;
+        if(dividend % divisor == 0) status = true;
         return status;
+    }
+
+    public static long greatest_prime_factor(long num_to_factor){
+        long max = 1;
+        
+        while(num_to_factor != 1){
+            int num = 2;
+            while(num < num_to_factor){
+                if(is_prime(num) && num_to_factor % num == 0){
+                    if(num > max) max = num;
+                    num_to_factor /= num;
+                    break;
+                }
+                num++;
+            }
+        }
+        return max;
     }
 }
